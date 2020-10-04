915c40249e4e7e57fa76c71eec62de956df54e5b
Vmeich <Victor.meichpro@gmail.com>
2020-10-04 15:15:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,23 +9,35 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculHash();
+        this.nonce = 0;
     } 
     // calcul le hash du bloc
     calculHash(){
-        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)+ this.nonce).toString();
+    }
+
+    Blockmining(Cooldown){
+        while(this.hash.substring(0,Cooldown)!== Array(Cooldown + 1).join("0")){
+            this.nonce++; 
+            this.hash = this.calculHash();
+        }
+
+        console.log("test de minage : " + this.hash);
     }
 }
 
 class Blockchain{
+        //chaque chaine créé commence automatiquement par un block "génèse" dont l'index est 0  
     constructor(){
         this.chain = [this.creationGenesisBlock()]; 
-         
+        this.Cooldown = 5; 
     }
+        // fonction de création du block génèse
     creationGenesisBlock(){
         return new Block(0, "01/01/2020", "GenesisBlock", "0");
     }
     
-    
+        // fonction pour trouver le dernier block créé 
     getBlockRécent(){
         return this.chain[this.chain.length - 1]; 
     }
@@ -33,13 +45,15 @@ class Blockchain{
         
     ajouterBlock(newBlock){
         newBlock.previousHash = this.getBlockRécent().hash; 
-        newBlock.hash = newBlock.calculHash(); 
+        newBlock.Blockmining(this.Cooldown);
         this.chain.push(newBlock);
     }
+    
+        //fonction pour voir si la chaine est valide = non modifié
     validationChain(){
        for(let i = 1; i < this.chain.length; i++){
            const currentBlock = this.chain[i];
-
+            
            if(currentBlock.hash !== currentBlock.calculHash()){
                return false;
            } 
@@ -52,11 +66,14 @@ class Blockchain{
         return true;
     }
 }
-
+ // création d'une nouvelle Blockchain    
 let Test1 = new Blockchain();
+
+console.log('minage du block 1')
 Test1.ajouterBlock(new Block(1, "10/02/2020", {amount: 3})) ;
+console.log('minage du block 2')
 Test1.ajouterBlock(new Block(2, "12/03/2020", {amount: 6})) ; 
 
-console.log('validé ? ' + Test1.validationChain());   
-
+// test dans terminal de création et Validation
+//console.log('validé ? ' + Test1.validationChain()); 
 //console.log(JSON.stringify(Test1, null, 4))
