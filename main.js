bd412793a371cf702c690c83c0c3ffc3347df1de
Vmeich <62282358+Vmeich@users.noreply.github.com>
2020-10-25 16:00:39 +0100

    Update main.js
    implémantation transaction et reward

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // implémente la librairie crypto-js
 const SHA256 = require('crypto-js/sha256');
 
+
+class transactions{
+    constructor(fromAddress, toAddress, Amount){
+        this.fromAddress = fromAddress ;
+        this.toAddress = toAddress ;
+        this.Amount = Amount ; 
+
+    }
+}
+
+
 //définit ce qu'est un block  
 class Block{
-    constructor (index, timestamp, data, previousHash=''){
-        this.index = index;
+    constructor (timestamp, transactions, previousHash=''){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculHash();
         this.nonce = 0;
@@ -30,25 +40,68 @@ class Blockchain{
         //chaque chaine créé commence automatiquement par un block "génèse" dont l'index est 0  
     constructor(){
         this.chain = [this.creationGenesisBlock()]; 
-        this.Cooldown = 5; 
+        this.Cooldown = 2;
+        this.cooldownTransactions = [] ;
+        this.Reward = 150;   
     }
         // fonction de création du block génèse
     creationGenesisBlock(){
-        return new Block(0, "01/01/2020", "GenesisBlock", "0");
+        return new Block("01/01/2020", "GenesisBlock", "0");
     }
     
         // fonction pour trouver le dernier block créé 
     getBlockRécent(){
         return this.chain[this.chain.length - 1]; 
     }
-
+                                                    //ancienne méthode
         
-    ajouterBlock(newBlock){
-        newBlock.previousHash = this.getBlockRécent().hash; 
-        newBlock.Blockmining(this.Cooldown);
-        this.chain.push(newBlock);
-    }
+    //ajouterBlock(newBlock){
+        //newBlock.previousHash = this.getBlockRécent().hash; 
+        //newBlock.Blockmining(this.Cooldown);
+        //this.chain.push(newBlock);
     
+                                                    // nouvelle méthode
+        
+    cooldownedTransactionsMining(rewardAddress){
+        let block = new Block(Date.now() ,this.cooldownTransactions) ;
+        block.Blockmining (this.Cooldown);
+
+        console.log("minage du block réussi");
+        this.chain.push(block);
+
+        this.cooldownTransactions =[
+            new transactions(null , rewardAddress , this.Reward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.cooldownTransactions.push(transaction);
+
+    }
+
+    getBalanceOfaddress(address){
+        let Balance = 0;
+
+        for(const Block of this.chain){
+            for(const trans of Block.transactions){
+                
+                if (trans.fromAddress === address){
+                    Balance -= trans.Amount ;
+                }
+
+                if (trans.toAddress === address){
+                    Balance += trans.Amount ;
+                }
+                
+
+            }
+        }
+        return Balance ;
+    }
+
+
+
+
         //fonction pour voir si la chaine est valide = non modifié
     validationChain(){
        for(let i = 1; i < this.chain.length; i++){
@@ -66,13 +119,23 @@ class Blockchain{
         return true;
     }
 }
+
  // création d'une nouvelle Blockchain    
 let Test1 = new Blockchain();
+Test1.createTransaction(new transactions('address1', 'address2', 100))
+Test1.createTransaction(new transactions('address2', 'address1', 10))
 
-console.log('minage du block 1')
-Test1.ajouterBlock(new Block(1, "10/02/2020", {amount: 3})) ;
-console.log('minage du block 2')
-Test1.ajouterBlock(new Block(2, "12/03/2020", {amount: 6})) ; 
+console.log('début du minage')
+Test1.cooldownedTransactionsMining('Vic-address');
+
+console.log('la balance de vic est :' , Test1.getBalanceOfaddress('Vic-adress'))
+
+console.log('début du minage')
+Test1.cooldownedTransactionsMining('Vic-address');
+
+console.log('la balance de vic est :' , Test1.getBalanceOfaddress('Vic-adress'))
+//console.log('minage du block 2')
+//Test1.ajouterBlock(new Block(2, "12/03/2020", {amount: 6})) ; 
 
 // test dans terminal de création et Validation
 //console.log('validé ? ' + Test1.validationChain()); 
